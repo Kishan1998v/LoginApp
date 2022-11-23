4efa05a217a955ca5296c5bfae32195db22cf845
kishan vishwakarma <99713347+Kishan1998v@users.noreply.github.com>
2022-11-23 13:45:25 +0530

    WE have setup the npm backend with nodemon and eslint

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="window">
+        <section id = "Details">
+        </section>
+        <SignIns/>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignIns from './components/SignIns.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignIns,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html{
+        width: 1280px;
+        height: 840px;
+    }
+    .window{
+        position: relative;
+        
+        background: #FFFFFF;
+    }
+    
+    #Details{    
+        /* Rectangle 1 */
+        position: absolute;
+        width: 526px;
+        height: 840px;
+        left: 0px;
+        top: 0px;
+        /* Neutral/Grey */
+        background: #404555;
+    }
 </style>
---
+++ front/src/components/SignIns.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name:'SignIn',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<style scoped>
+    
+
+</style>>
